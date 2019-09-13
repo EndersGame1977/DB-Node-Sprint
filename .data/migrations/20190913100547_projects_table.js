090e393f0a61cc1a553b21e4bdeaf5d72e986cf6
Jason Prince <jasoncprince@gmail.com>
2019-09-13 10:44:47 -0600

    Cofigured migration projects table

--- .data/migrations/20190913100547_projects_table.js
+++ .data/migrations/20190913100547_projects_table.js
@@ -1,8 +1,15 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("projects", column => {
+    column.increments(); // Unique ID
+    column.string("projectName").notNullable(); // Name is required
+    column.string("description"); // Description is not required
+    column
+      .boolean("completed")
+      .notNullable()
+      .defaultTo(false); // Completed is required and default set to false
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.dropTableIfExists("projects");
 };
