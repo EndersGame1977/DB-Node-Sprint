658aec3a3b5a7ec1a0f121243a1b31266447b80c
Jason Prince <jasoncprince@gmail.com>
2019-09-13 13:26:42 -0600

    Created db-config

---
+++ projects/projects-routers.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const Projects = require("./projects-modules.js");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  Projects.find()
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(err => {
+      res.status(500).json({ message: err.message });
+    });
+});
