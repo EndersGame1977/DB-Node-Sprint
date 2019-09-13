ea556841f673d071cb1ccc63016db6bc74d9045d
Jason Prince <jasoncprince@gmail.com>
2019-09-13 11:01:25 -0600

    Made name for projects and resources unique

--- .data/migrations/20190913100547_projects_table.js
+++ .data/migrations/20190913100547_projects_table.js
@@ -1,7 +1,10 @@
 exports.up = function(knex) {
   return knex.schema.createTable("projects", column => {
     column.increments(); // Unique ID
-    column.string("projectName").notNullable(); // Name is required
+    column
+      .string("projectName")
+      .notNullable()
+      .unique(); // Name is required and unique
     column.string("description"); // Description is not required
     column
       .boolean("completed")
--- .data/migrations/20190913101021_resources_table.js
+++ .data/migrations/20190913101021_resources_table.js
@@ -1,7 +1,10 @@
 exports.up = function(knex) {
   return knex.schema.createTable("resources", column => {
     column.increments(); // Unique ID
-    column.string("resourcesName").notNullable(); // Name is required
+    column
+      .string("resourcesName")
+      .notNullable()
+      .unique(); // Name is required and unique
     column.string("descripton"); // description is not required
     column.integer("tasks_id").notNullable(); //Used to join to tasks table
     column
