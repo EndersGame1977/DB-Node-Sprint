63ebba80c08713500d0779efa7f0ea0f6dbccd3d
Jason Prince <jasoncprince@gmail.com>
2019-09-13 11:26:35 -0600

    Configured migrations projects-resources

--- .data/migrations/20190913111654_projects-resources_table.js
+++ .data/migrations/20190913111654_projects-resources_table.js
@@ -1,8 +1,24 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("projects-resources", column => {
+    column
+      .integer("projects_id")
+      .unsigned()
+      .references("id")
+      .inTable("projects")
+      .onDelete("CASCADE") // if the PK record is deleted
+      .onUpdate("CASCADE"); // if the PK value updates
+    column
+      .integer("resources_id")
+      .unsigned()
+      .references("id")
+      .inTable("resources")
+      .onDelete("CASCADE") // if the PK record is deleted
+      .onUpdate("CASCADE"); // if the PK value updates
+
+    column.primary(["projects_id", "resources_id"]);
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.dropTableIfExists("projects-resources");
 };
