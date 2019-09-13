aaeb3807a2bbe5e30fe0314116e96692013d3052
Jason Prince <jasoncprince@gmail.com>
2019-09-13 13:39:02 -0600

    Server is running

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 
-const ProjectRouter = require("./schemes/project-router.js");
+const ProjectRouter = require("./projects/projects-routers.js");
 
 const server = express();
 
 server.use(express.json());
-server.use("/api/projects", ProjectRouter);
+server.use("/projects", ProjectRouter);
 
 module.exports = server;
