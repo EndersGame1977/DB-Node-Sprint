eca06891656f4c688a899900a7cb8aa4ce4d769b
Jason Prince <jasoncprince@gmail.com>
2019-09-13 10:51:07 -0600

    Cofigured migration tasks table

--- .data/migrations/20190913100904_tasks_table.js
+++ .data/migrations/20190913100904_tasks_table.js
@@ -1,8 +1,16 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("tasks", column => {
+    column.increments(); // Unique ID
+    column.string("description").notNullable(); // Description is required
+    column.string("notes"); // Notes is not required
+    column.integer("projects_id").notNullable(); //Used to join to projects table
+    column
+      .boolean("completed")
+      .notNullable()
+      .defaultTo(false); // Completed is required and default set to false
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.dropTableIfExists("tasks");
 };
