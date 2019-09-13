b0fe52f86ff6ce45464d3873307959bc74e0fa76
Jason Prince <jasoncprince@gmail.com>
2019-09-13 12:23:40 -0600

    Configured tasks seed

--- .data/seeds/02_tasks.js
+++ .data/seeds/02_tasks.js
@@ -5,9 +5,46 @@ exports.seed = function(knex) {
     .then(function() {
       // Inserts seed entries
       return knex("tasks").insert([
-        { id: 1, colName: "rowValue1" },
-        { id: 2, colName: "rowValue2" },
-        { id: 3, colName: "rowValue3" }
+        { step: 1, description: "Turn off car.", projects_id: 1 },
+        {
+          step: 2,
+          description:
+            "Use screwdriver to disconnect terminals from old battery.",
+          projects_id: 1
+        },
+        {
+          step: 3,
+          description: "Remove old battery from car.",
+          projects_id: 1
+        },
+        { step: 4, description: "Place new battery in car.", projects_id: 1 },
+        {
+          step: 5,
+          description: "Connect terminals to battery using screwdriver.",
+          projects_id: 1
+        },
+        { step: 6, description: "Turn on car.", projects_id: 1 },
+        { step: 1, description: "Turn off laptop.", projects_id: 2 },
+        {
+          step: 2,
+          description:
+            "Using screwdriver, remove screws holding back cover of laptop.",
+          projects_id: 2
+        },
+        { step: 3, description: "Unplug fan.", projects_id: 2 },
+        {
+          step: 4,
+          description: "Using can of compressed air, blow dust out of fan.",
+          projects_id: 2
+        },
+        { step: 5, description: "Plug fan back in.", projects_id: 2 },
+        {
+          step: 6,
+          description:
+            "Using screwdriver, screw screws for back cover back in.",
+          projects_id: 2
+        },
+        { step: 7, description: "Turn on laptop.", projects_id: 2 }
       ]);
     });
 };
