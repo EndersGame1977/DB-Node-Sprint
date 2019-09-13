5bb1ea457113cb1005bdeebeb3d11b637ee0e48a
Jason Prince <jasoncprince@gmail.com>
2019-09-13 12:32:33 -0600

    Removed 'completed' from migration resources

--- .data/migrations/20190913101021_resources_table.js
+++ .data/migrations/20190913101021_resources_table.js
@@ -6,10 +6,6 @@ exports.up = function(knex) {
       .notNullable()
       .unique(); // Name is required and unique
     column.string("descripton"); // description is not required
-    column
-      .boolean("completed")
-      .notNullable()
-      .defaultTo(false); // Completed is required and default set to false
     // Forgien key
     column
       .integer("tasks_id")
