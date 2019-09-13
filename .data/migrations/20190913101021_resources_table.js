e83411004b8ee9e8225f6737e32a1295e7f51b01
Jason Prince <jasoncprince@gmail.com>
2019-09-13 10:54:37 -0600

    Cofigured migration resources table

--- .data/migrations/20190913101021_resources_table.js
+++ .data/migrations/20190913101021_resources_table.js
@@ -1,8 +1,16 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("resources", column => {
+    column.increments(); // Unique ID
+    column.string("resourcesName").notNullable(); // Name is required
+    column.string("descripton"); // description is not required
+    column.integer("tasks_id").notNullable(); //Used to join to tasks table
+    column
+      .boolean("completed")
+      .notNullable()
+      .defaultTo(false); // Completed is required and default set to false
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.dropTableIfExists("resources");
 };
